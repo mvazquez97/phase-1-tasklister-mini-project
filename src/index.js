2874b4b95f4b7fc0f1e2c5e4d8d0c42010992f77
Manuel <manuelvazquez76@yahoo.com>
2023-05-13 12:24:50 -0700

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+addingEventListeners()
+})
+
+function addingEventListeners() {
+  document.getElementById("create-task-form").addEventListener("submit", handleFormSubmit)
+
+}
+
+ function handleFormSubmit(e) {
+e.preventDefault()
+  const task = e.target[0].value
+
+  displayTask(task)
+}
+
+  
+
+function displayTask(task) {
+const taskUl = document.getElementById("tasks")
+
+const taskLi = document.createElement("li")
+const deleteBtn = document.createElement("button")
+
+
+
+deleteBtn.textContent = "X"
+deleteBtn.addEventListener("click", deleteTask)
+taskLi.textContent = task + " "
+
+
+taskLi.appendChild(deleteBtn)
+taskUl.appendChild(taskLi)
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
